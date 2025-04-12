cc237a57e039eb4955aec2f73406e7b6cfd40cb5
aryanprince13 <aryankumarprince13@gmail.com>
2025-04-12 13:17:21 +0530

    Initial commit

---
+++ cpp/temp.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    // int a = 1;
+    // int b (2);
+    // // int c{3};
+
+    // char d = 'a';
+    // char e ('b');
+
+    // cout<<a<<endl;
+    // cout<<b<<endl;
+    // cout<<d<<endl;
+
+     
+    // cout<<e<<endl;
+
+    string stri = "abhinav";
+    string atri = "abhinav";
+
+    // cout<<stri<<endl;
+    // cout<<atri<<endl;
+    // cout<<stri + atri <<endl;
+    // cout<< atri.length() <<endl;
+    // cout<<stri.size()<<endl;
+    // cout<<stri.capacity()<<endl;
+    cout<< stri.compare(atri)<<endl;
+    
+
+}
